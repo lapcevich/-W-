c18e80f7ad1fe7d1e05107bb74d8335508560d43
Nikita <nikitlapcevich@gmail.com>
2025-01-23 22:08:02 +0300

    Lab7

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import React, { useContext } from "react";
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
-import { AuthContext, AuthProvider } from "./AuthContext";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
 import Login from "./Login";
 import Register from "./Register";
+import DataTable from "./DataTable";
 import Home from "./Home";
-import DataTable from "./DataTable"; // Импортируем DataTable
-import { AppBar, Toolbar } from "@mui/material";
 
-const PrivateRoute = ({ children }) => {
-  const { isAuthenticated } = useContext(AuthContext);
-  return isAuthenticated() ? children : <Navigate to="/login" />;
-};
-
-function App() {
+const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <AppBar position="static">
-          <Toolbar>
-            <Link to="/" style={{ color: 'white', textDecoration: 'none', marginRight: '20px' }}>Home</Link>
-            <Link to="/data-table" style={{ color: 'white', textDecoration: 'none', marginRight: '20px' }}>Data Table</Link> {/* Добавляем ссылку на DataTable */}
-            <Link to="/login" style={{ color: 'white', textDecoration: 'none', marginRight: '20px' }}>Login</Link>
-            <Link to="/register" style={{ color: 'white', textDecoration: 'none' }}>Register</Link>
-          </Toolbar>
-        </AppBar>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/data-table" element={<DataTable />} /> {/* Добавляем маршрут для DataTable */}
+          <Route path="/data-table" element={<DataTable />} />
         </Routes>
       </Router>
     </AuthProvider>
   );
-}
+};
 
 export default App;
